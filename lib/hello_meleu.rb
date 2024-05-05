da23a9cad0a3906a54b3df6d1208f63d37398193
Augusto Lopes <meleu.dev@gmail.com>
2024-05-04 23:54:11 -0300

    doc: add documentation

--- lib/hello_meleu.rb
+++ lib/hello_meleu.rb
@@ -1,4 +1,11 @@
+# Class used to greet meleu with "hello".
+#
+# @examples
+# HelloMeleu.hello #=> "Hello meleu!"
 class HelloMeleu
+  # Greets meleu with "hello".
+  #
+  # @return [String] "Hello meleu!"
   def self.hello
     'Hello meleu!'
   end
